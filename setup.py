b01545ec03cd230a88c20b2e4ab9d94dd600c2b1
Santiago <santiago.basaldua@bbvadata.com>
2025-02-04 12:29:56 +0100

    Enhances packaging to include notebooks

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+import os, shutil
+
+
+# Move notebooks inside src/settrie before packaging
+if os.path.exists('notebooks'):
+	shutil.move('notebooks', 'src/settrie/tutorials')
+
+
 from setuptools import setup, find_packages, Extension
 
 
@@ -7,10 +15,12 @@ settrie_ext = Extension(name				= 'settrie._py_settrie',
 						extra_compile_args	= ['-std=c++11', '-c', '-fpic', '-O3'])
 
 setup_args = dict(
-	packages		= find_packages(where = 'src'),
-	package_dir		= {'' : 'src'},
-	ext_modules		= [settrie_ext],
-	scripts			= ['src/test_all.py']
+	packages			 = find_packages(where = 'src'),
+	package_dir			 = {'' : 'src'},
+	ext_modules			 = [settrie_ext],
+	scripts				 = ['src/test_all.py'],
+	include_package_data = True,
+	package_data		 = {'mypackage': ['tutorials/*', 'tutorials/data/*']}
 )
 
 setup(**setup_args)
